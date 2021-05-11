b5e2f7f52e15b689b71d1d9c1edf019190b665cf
Paco-Murillo <jfmurillol@gmail.com>
2021-05-11 18:07:52 -0500

    Agregué el checar BotAvgPercentage, con la función calculateBotAvgPercentage, que recibe el api, el diputado a analizar y el Botometer ya inicializado en main()

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import botometer
 import tweepy
 from textblob import TextBlob
 import unicodedata
@@ -93,10 +94,31 @@ def calculateMaxMinEngagement(api, cuentaDeDiputado):
     print("Tweets analizados = " + str(count))
     print("Max Engagement = " + str(max))
     print("Avg Engagement = " + str(avgEngagement/count))
+    
+def calculateBotPercentageAvg(api, cuentaDeDiputado, botometer):
+
+    MAX_FOLLOWERS = 1000
+    listAccounts = []
+    for follower in tweepy.Cursor(api.followers, id=cuentaDeDiputado,).items(10):
+        followerID = follower._json['screen_name']
+        listAccounts.append(str('@' + followerID))
+        if len(listAccounts) == MAX_FOLLOWERS:
+            break
+
+    print(listAccounts)
+    avgBot = 0
+    for _, result in botometer.check_accounts_in(listAccounts):
+        avgBot += int(result['raw_scores']['universal']['fake_follower'])
+
+    count = len(listAccounts)
+    print("Cuenta analizada = " + cuentaDeDiputado)
+    print("Followers analizados = " + str(count))
+    print("Avg Engagement = " + str(avgBot / count))
 
 
 def main():
 
+    # - - - Solo cambien el nombre de las cuentas aquí
     # ---------------- Cuenta entre comillas dobles, sin @
     cuentaDeDiputado = "Marianavalto"
     # -------------------- ^^^^
@@ -107,13 +129,28 @@ def main():
     access_token = "1383461526200209412-HXTe94Jo7PphohvME7CkmBfQm4yg9u"
     access_token_secret = "I9nQothxyDWdkji08wZZEjIoUOZuk9v6PMbP4vMcwzxEE"
 
+    # --------- RapidAPI
+
+    rapidapi_key = "398be2ebeemsh6a1555df87ce3e1p16985djsnf04e4880bd62"
+    twitter_app_auth = {
+        'consumer_key': consumer_key,
+        'consumer_secret': consumer_secret,
+        'access_token': access_token,
+        'access_token_secret': access_token_secret,
+    }
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    bom = botometer.Botometer(wait_on_ratelimit=True,
+                              rapidapi_key=rapidapi_key,
+                              **twitter_app_auth)
 
-    getTweets(api, cuentaDeDiputado)
+    # getTweets(api, cuentaDeDiputado)
     # calculateMaxMinEngagement(api, cuentaDeDiputado)
 
+    # La nueva función vvvvvvvv
+    calculateBotPercentageAvg(api, cuentaDeDiputado, bom)
 
 main()
